d32ab013769ce6981e7869144969c11268a61c25
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-03-21 12:14:15 -0300

    styling Game component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 export function Footer() {
     return (
         <footer>
-            <p>Desenvolvido por <a href="https://github.com/ABeatrizSC">ABeatrizSC</a>.</p>
+            <p>Desenvolvido por <a target='_blank' href="https://github.com/ABeatrizSC">ABeatrizSC</a>.</p>
         </footer>
     )
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,8 +2,29 @@ import './Game.css'
 
 export function Game({ verifyLetter}) {
     return (
-        <div>
-            <button onClick={verifyLetter}>Finalizar jogo</button>
+        <div className='game-container'>
+            <p>Pontuação: <span className="points">000</span></p>
+            <h2>Adivinhe a palavra: </h2>
+            <p>Dica: <span className='tip'>Comida</span></p>
+            <p>Você ainda possui <span>X</span> tentativa(s).</p>
+            <div className="word-container">
+                <span className='letter'>A</span>
+                <span className="blank-square"></span>
+            </div>
+            <div className="letter-container">
+                <p>Tente adivinhar uma letra da palavra:</p>
+                <form className='form'>
+                    <input type="text" maxLength={1} required/>
+                    <button>Enviar</button>
+                </form>
+            </div>
+            <div className='wrong-letters-container'>
+                <p>Letras já utilizadas:</p>
+                <div>
+                    <span>A, </span>
+                    <span>B, </span>
+                </div>
+            </div>
         </div>
     )
 }
