e78dd526c4abcace8b18f7bf1def0287857b2664
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-03-23 12:53:55 -0300

    add new categories and words in data.js

--- src/data/words.js
+++ src/data/words.js
@@ -1,8 +1,12 @@
 export const wordsList = {
-    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
-    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
-    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
-    programação: ["Linguagem", "Framework", "JavaScript", "React"],
-    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+    carro: ["Motor", "Porta", "Capo", "Pneu", "Antena", "Volante", "Bancos", "Retrovisor", "Farol", "Freio", "Rodas", "Cinto", "Rádio"],
+    fruta: ["Banana", "Maca", "Pera", "Mamao", "Laranja", "Abacaxi", "Morango", "Melancia", "Uva", "Kiwi", "Limao", "Goiaba", "Pessego", "Coco", "Melao"],
+    corpo: ["Braco", "Perna", "Cerebro", "Pescoco", "Olhos", "Orelha", "Barriga", "Cabelo", "Pe", "Mao", "Dedo", "Joelho", "Dente", "Lingua", "Nariz"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "PlacaMae", "Processador", "Memoria", "DiscoRigido", "Fonte", "PlacaDeVideo", "Cooler", "Webcam", "SSD", "Cabos"],
+    programacao: ["Linguagem", "Framework", "JavaScript", "React", "Python", "CSS", "HTML", "Nodejs", "Angular", "Vuejs", "Ruby", "PHP", "API", "Biblioteca", "Backend"],
+    alimento: ["Arroz", "Feijao", "Carne", "Leite", "Ovo", "Pao", "Queijo", "Macarrao", "Batata", "Peixe", "Frango", "Iogurte", "Alface", "Cenoura", "Azeite"],
+    cidade: ["Rio", "SaoPaulo", "NovaYork", "Londres", "Paris", "Toquio", "Berlim", "Roma", "Pequim", "Moscou", "Dubai", "Sydney", "Cairo", "Toronto", "Madrid"],
+    animal: ["Cachorro", "Gato", "Elefante", "Leao", "Tigre", "Girafa", "Zebra", "Panda", "Coelho", "Urso", "Macaco", "Hipopotamo", "Gorila", "Rato", "Camelo"],
+    pais: ["Brasil", "EstadosUnidos", "Franca", "Japao", "Canada", "Alemanha", "Italia", "China", "India", "Australia", "Argentina", "Mexico", "Portugal", "Russia", "Coreia"],
+    cor: ["Vermelho", "Azul", "Verde", "Amarelo", "Roxo", "Branco", "Preto", "Laranja", "Marrom", "Cinza", "Rosa", "Dourado", "Prateado", "Bege", "Turquesa"],
 };
